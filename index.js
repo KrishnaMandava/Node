0cf985f824db35b58217515a8af45a2fc691b394
root <root@localhost.localdomain>
2020-09-26 16:33:21 +0530

    Intial Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// Regular Functions
+
+// add is function name
+// x,y are parameters
+function add(x,y){
+    console.log(x+y)
+}
+
+// mul is function name
+// x,y are parameters
+function multiply(x,y){
+    console.log(x*y)
+}
+
+add(5,5)
+multiply(1,2)
+
+
+// Arrow function
+
+// Here --sum-- is function name
+//x, y are parameters
+const sum = (x,y) => {
+    console.log(x+y)
+}
+
+//Here --mul-- is function name
+// x,y are parameters
+const mul = (x,y) => {
+    console.log(x*y)
+}
+
+
