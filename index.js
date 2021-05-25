bc12b236403a227c9f91226a102992edc5f600a1
Krishna Mandava <mandava.krishna777@gmail.com>
2021-05-25 22:42:49 +0530

    my commi

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-// Regular Functions
-
-// add is function name
-// x,y are parameters
-function add(x, y) {
-  console.log(x + y);
+function login() {
+  //
 }
-
-// mul is function name
-// x,y are parameters
-function multiply(x, y) {
-  console.log(x * y);
-}
-
-add(5, 5);
-multiply(1, 2);
-
-// Arrow function
-
-// Here --sum-- is function name
-//x, y are parameters
-const sum = (x, y) => {
-  console.log(x + y);
-};
-
-//Here --mul-- is function name
-// x,y are parameters
-const mul = (x, y) => {
-  console.log(x * y);
-};
