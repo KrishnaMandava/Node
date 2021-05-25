9f31f8213ac0d196ec0e578b80518a742db66762
Sravani <regulasravani112@gmail.com>
2021-05-25 22:47:02 +0530

    s

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-function login() {
-  //
+// Regular Functions
+
+// add is function name
+// x,y are parameters
+function add(x, y) {
+  console.log(x + y);
 }
+
+// mul is function name
+// x,y are parameters
+function multiply(x, y) {
+  console.log(x * y);
+}
+
+add(5, 5);
+multiply(1, 2);
+
+// Arrow function
+
+// Here --sum-- is function name
+//x, y are parameters
+const sum = (x, y) => {
+  console.log(x + y);
+};
+
+//Here --mul-- is function name
+// x,y are parameters
+const mul = (x, y) => {
+  console.log(x * y);
+};
